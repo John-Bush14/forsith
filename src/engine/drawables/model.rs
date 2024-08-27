d1c65e5370fd4932d5580e6023cbe28026e86548
John-Bush14 <joppe@stuyck.be>
2024-08-27 13:56:57 +0200

    feat model_from_obj: importing multiple models from 1 file

--- src/engine/drawables/model.rs
+++ src/engine/drawables/model.rs
@@ -9,40 +9,40 @@ impl Drawable {
         drawable.pipeline_id = super::PIPELINE_3D;
 
 
-        let (models, _) = tobj::load_obj(std::path::Path::new(file)).unwrap();
-        let mesh = &models[0].mesh;
+        drawable.vertices = vec!();
+        drawable.coords = vec!();
 
-        let positions = mesh.positions.as_slice();
-        let coords = mesh.texcoords.as_slice();
-        let indices = mesh.indices.as_slice();
+        let (mut models, _) = tobj::load_obj(std::path::Path::new(file)).unwrap();
+        while let Some(model) = models.pop() {
+            let mesh = model.mesh;
 
+            let positions = mesh.positions.as_slice();
+            let coords = mesh.texcoords.as_slice();
+            let indices = mesh.indices.as_slice();
 
-        drawable.vertices = Vec::with_capacity(indices.len());
-        drawable.coords = Vec::with_capacity(indices.len());
+            for indice in indices {
+                if *indice as usize >= positions.len() {panic!("what the fuck! {:?}", indice);}
 
+                let pi = (indice * 3) as usize;
+                let ci = (indice * 2) as usize;
 
-        for indice in indices {
-            if *indice as usize >= positions.len() {panic!("what the fuck! {:?}", indice);}
+               let x = positions[pi];
+                let y = positions[pi + 1];
+                let z = positions[pi + 2];
+                let u = coords[ci];
+               let v = coords[ci + 1];
 
-            let pi = (indice * 3) as usize;
-            let ci = (indice * 2) as usize;
+                drawable.vertices.push(Vertex {
+                    pos: [x, y, z],
+                    coord: [0.0;2],
+                    color: [1.0;4]
+               });
 
-            let x = positions[pi];
-            let y = positions[pi + 1];
-            let z = positions[pi + 2];
-            let u = coords[ci];
-            let v = coords[ci + 1];
-
-            drawable.vertices.push(Vertex {
-                pos: [x, y, z],
-                coord: [0.0;2],
-                color: [1.0;4]
-            });
-
-            drawable.coords.push([
-                u,
-                v
-            ]);
+                drawable.coords.push([
+                    u,
+                    v
+                ]);
+            }
         }
 
 
