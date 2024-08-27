a0874bc26d7a63c917575efec7057fad3050101d
John-Bush14 <joppe@stuyck.be>
2024-08-27 14:05:07 +0200

    fix: flipv on model instead of on image

--- src/engine/drawables/model.rs
+++ src/engine/drawables/model.rs
@@ -20,27 +20,30 @@ impl Drawable {
             let coords = mesh.texcoords.as_slice();
             let indices = mesh.indices.as_slice();
 
+            drawable.vertices.reserve(indices.len());
+            drawable.coords.reserve(indices.len());
+
             for indice in indices {
                 if *indice as usize >= positions.len() {panic!("what the fuck! {:?}", indice);}
 
                 let pi = (indice * 3) as usize;
                 let ci = (indice * 2) as usize;
 
-               let x = positions[pi];
+                let x = positions[pi];
                 let y = positions[pi + 1];
                 let z = positions[pi + 2];
                 let u = coords[ci];
-               let v = coords[ci + 1];
+                let v = coords[ci + 1];
 
                 drawable.vertices.push(Vertex {
                     pos: [x, y, z],
                     coord: [0.0;2],
                     color: [1.0;4]
-               });
+                });
 
                 drawable.coords.push([
                     u,
-                    v
+                    1.0 - v
                 ]);
             }
         }
