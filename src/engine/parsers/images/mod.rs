b9784427942b353ac260ea15941e7dc8b383da93
John-Bush14 <joppe@stuyck.be>
2024-09-10 17:17:33 +0200

    feat: general reading utility functions

--- src/engine/parsers/images/mod.rs
+++ src/engine/parsers/images/mod.rs
@@ -1,14 +1,27 @@
 use std::path::Path;
 use std::ffi::OsStr;
+use std::fs::File;
+use std::io::Read;
+use crate::DynError;
 
 
 mod ppm;
 
 
 pub trait ImageParser {
-    fn parse(file: &Path) -> Result<((u32, u32), u64, Vec<u8>), Box<dyn std::error::Error>>;
+    fn parse(file: &Path) -> Result<((u32, u32), u64, Vec<u8>), DynError>;
 }
 
+
+pub(crate) fn read_bytes<T: AsMut<[u8]>>(file: &mut File, mut buffer: T) -> Result<T, DynError> {
+    file.read_exact(buffer.as_mut())?;
+
+    return Ok(buffer);
+}
+
+pub(crate) fn read_decimal_int(file: &File) -> Result<u16, DynError> {return Ok(read_decimal_word(file)?.parse::<u16>()?);}
+
+pub(crate) fn read_decimal_word(file: &File) -> Result<String, DynError> {todo!();}
 
 #[allow(unreachable_code, dead_code, non_snake_case)]
 pub fn parse_image(file: &Path) -> Result<((u32, u32), u64, Vec<u8>), Box<dyn std::error::Error>> {
