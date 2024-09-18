15848b8bb8f414ea86fbd9cebd119ac03c50ac82
John-Bush14 <joppe@stuyck.be>
2024-09-18 17:01:59 +0200

    REWORK (v0.1.0 -> v1.1.0)

--- src/engine/parsers/mod.rs
+++
@@ -1,5 +0,0 @@
-mod images; mod models;
-
-pub use images::parse_image;
-
-pub use models::parse_model;
--- src/vulkan/devices/mod.rs
+++
@@ -1,5 +0,0 @@
-pub use super::{CString, c_void, c_char, vk_make_version, VkStructureType};
-
-pub mod device;
-
-pub mod physical_device;
