151858ee5fedae473f2ec15ac4118f91de917418
John-Bush14 <joppe@stuyck.be>
2024-09-19 23:03:59 +0200

    feat: vkStructureType type + some consts

---
+++ forsith-bindings/src/structure_type.rs
@@ -0,0 +1,10 @@
+use crate::VkEnum;
+
+
+pub type VkStructureType = VkEnum;
+
+
+crate::pub_consts!(VkStructureType,
+    VK_STRUCTURE_TYPE_APPLICATION_INFO = 0,
+    VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO = 1,
+);
