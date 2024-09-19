3c9b1c7b0e067e65531192ba9d1050898571836a
John-Bush14 <joppe@stuyck.be>
2024-09-19 23:06:15 +0200

    feat: vulkan_app creation test

--- forsith-engine/src/E2E_tests.rs
+++ forsith-engine/src/E2E_tests.rs
@@ -1,4 +1,11 @@
 /// open's up window and tries to show as many features as possible,
 /// programmer is supposed to check if these features show up as test.
+#[cfg(test)]
 mod visual_test {
+    use crate::{vk_make_version, vulkan_app::VulkanApp};
+
+    #[test]
+    fn test() {
+        let vulkan_app = VulkanApp::new("test", vk_make_version(0, 0, 0));
+    }
 }
