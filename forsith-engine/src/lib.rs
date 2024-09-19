809048d51739000a1f5b64a52de4d0e10601b42f
John-Bush14 <joppe@stuyck.be>
2024-09-19 23:02:26 +0200

    feat: engine name/version consts

--- forsith-engine/src/lib.rs
+++ forsith-engine/src/lib.rs
@@ -1,11 +1,24 @@
+use bindings::VkVersion;
+
+
 mod drop;
 
 #[allow(non_snake_case)]
 mod E2E_tests;
-
 
 pub mod drawable;
 pub mod render_target;
 pub mod vulkan_app;
 pub mod world_view;
 pub mod uniform_object;
+
+
+pub const ENGINE_NAME: &str = "forsith";
+
+pub const ENGINE_VERSION: fn() -> VkVersion = || vk_make_version(0, 1, 0);
+pub const API_VERSION: fn() -> VkVersion = || vk_make_version(1, 0, 0);
+
+
+pub fn vk_make_version(major: u32, minor: u32, patch: u32) -> VkVersion {
+    return (major << 22) | (minor << 12) | patch;
+}
