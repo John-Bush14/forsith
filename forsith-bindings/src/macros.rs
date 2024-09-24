ddb465e42c8c43306cbfb0ddc27cbd65a4eb008c
John-Bush14 <joppe@stuyck.be>
2024-09-24 23:05:12 +0200

    feat(tested): define_vk_struct! macro

--- forsith-bindings/src/macros.rs
+++ forsith-bindings/src/macros.rs
@@ -27,6 +27,33 @@ macro_rules! define_vk_enum {
     };
 }
 
+#[macro_export]
+macro_rules! define_vk_struct {
+    ($visibility:ident $struct:ident {$($field:ident: $type:ty $(,)? )*}) => {
+        paste::item! {
+            #[repr(C)]
+            $visibility struct $struct {
+                $($visibility [<$field:snake>]: $type,)*
+            }
+        }
+    };
+
+    ($visibility:ident $struct:ident($structure_type:ident) {$($field:ident: $type:ty $(,)? )*}) => {
+        #[allow(dead_code)]
+        use crate::structure_type::VkStructureType;
+
+        crate::define_vk_struct!($visibility $struct {
+            s_type: VkStructureType,
+            p_next: *const std::ffi::c_void,
+            $($field: $type,)*
+        });
+
+        paste::item! {impl $struct {
+            $visibility fn structure_type() -> VkStructureType {VkStructureType::[<$structure_type:camel>]}
+        }}
+    };
+}
+
 
 #[cfg(test)]
 mod macro_tests {
@@ -57,5 +84,31 @@ mod macro_tests {
 
         assert!(alive.contains(TestBitflag::Alive));
         assert!(!alive.contains(TestBitflag::Dead));
+    }
+
+
+    define_vk_struct!(
+        pub TestVkStruct {
+            field: *const u32
+        }
+    );
+
+    define_vk_struct!(
+        pub TestVkCreateStruct(VK_STRUCTURE_TYPE_APPLICATION_INFO) {
+            field: *const u32
+        }
+    );
+
+    #[test]
+    fn use_defined_vk_struct() {
+        let test_vk_create_struct = TestVkCreateStruct {
+            s_type: TestVkCreateStruct::structure_type(),
+            p_next: std::ptr::null(),
+            field: std::ptr::null(),
+        };
+
+        assert!(test_vk_create_struct.s_type as u32 == VkStructureType::VkStructureTypeApplicationInfo as u32);
+
+        let _test_vk_struct = TestVkStruct {field: std::ptr::null()};
     }
 }
