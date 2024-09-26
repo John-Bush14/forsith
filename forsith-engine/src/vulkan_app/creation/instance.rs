b18d0df1210f863d7de486ed97dc122851d8bbef
John-Bush14 <joppe@stuyck.be>
2024-09-26 12:41:22 +0200

    feat: create_instance()

---
+++ forsith-engine/src/vulkan_app/creation/instance.rs
@@ -0,0 +1,41 @@
+use bindings::{instance::{vkCreateInstance, VkApplicationInfo, VkInstance, VkInstanceCreateFlags, VkInstanceCreateInfo}, vk_version, VkVersion};
+use std::ffi::CString;
+use super::API_VERSION;
+
+
+pub fn create_instance(app_name: &str, app_version: VkVersion) -> VkInstance {
+    let c_app_name = CString::new(app_name).expect("invalid app name");
+
+    let engine_name = CString::new("forsith").unwrap();
+
+
+    let app_info = VkApplicationInfo {
+        s_type: VkApplicationInfo::structure_type(),
+        p_next: std::ptr::null(),
+        application_name: c_app_name.as_ptr(),
+        application_version: app_version,
+        engine_name: engine_name.as_ptr(),
+        engine_version: vk_version(0, 1, 0),
+        api_version: API_VERSION(),
+    };
+
+
+    let instance_info = VkInstanceCreateInfo {
+        s_type: VkInstanceCreateInfo::structure_type(),
+        p_next: std::ptr::null(),
+        flags: VkInstanceCreateFlags(0),
+        application_info: &app_info as *const VkApplicationInfo,
+        enabled_layer_count: 0,
+        enabled_layer_names: std::ptr::null(),
+        enabled_extension_count: 0,
+        enabled_extensions: std::ptr::null()
+    };
+
+
+    let mut instance = 0;
+
+    unsafe {vkCreateInstance(&instance_info as *const VkInstanceCreateInfo, std::ptr::null(), &mut instance)};
+
+
+    return instance;
+}
--- forsith-engine/src/vulkan_app/creation/mod.rs
+++ forsith-engine/src/vulkan_app/creation/mod.rs
@@ -4,13 +4,20 @@ use super::VulkanApp;
 use bindings::vk_version;
 
 
+mod instance;
+
+
 #[allow(dead_code)]
 pub const API_VERSION: fn() -> VkVersion = || vk_version(1, 0, 0);
 
 
 #[allow(dead_code)]
 impl VulkanApp {
-    fn new(_app_name: &str, _app_version: VkVersion) -> Result<Self, DynError> {todo!();}
+    fn new(app_name: &str, app_version: VkVersion) -> Result<Self, DynError> {
+        let instance = instance::create_instance(app_name, app_version);
+
+        todo!();
+    }
 }
 
 
