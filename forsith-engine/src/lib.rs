ec7cdfab39725bb69067ef29291605154c69ab22
John-Bush14 <joppe@stuyck.be>
2024-09-26 21:49:38 +0200

    feat: ENGINE_VERSION -> automatic

--- forsith-engine/src/lib.rs
+++ forsith-engine/src/lib.rs
@@ -7,7 +7,14 @@ pub mod vulkan_app;
 
 pub const API_VERSION: fn() -> VkVersion = || vk_version(1, 0, 0);
 
-pub const ENGINE_VERSION: fn() -> VkVersion = || vk_version(0, 1, 0);
+pub const ENGINE_VERSION: fn() -> VkVersion = || {
+    let [major, minor, patch] = env!("CARGO_PKG_VERSION").split('.')
+        .map(|num| return num.parse::<u32>().expect("incorrect crate version format (incorrect num)"))
+        .collect::<Vec<u32>>()
+        .try_into().expect("incorrect crate version format (to much nums)");
+
+    return vk_version(major, minor, patch);
+};
 
 pub const ENGINE_NAME: fn() -> CString = || CString::new("forsith").unwrap();
 
