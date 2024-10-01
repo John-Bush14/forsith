348e840ce2e6b7cc41b2bff5cf9dbe16de3e529e
John-Bush14 <joppe@stuyck.be>
2024-10-01 21:11:19 +0200

    feat: rate_device_type

--- forsith-engine/src/vulkan_app/device.rs
+++ forsith-engine/src/vulkan_app/device.rs
@@ -1,4 +1,4 @@
-use bindings::{device::VkDevice, physical_device::{VkPhysicalDevice, VkQueue, VkQueueFamily, VkQueueFlagBits}};
+use bindings::{device::VkDevice, physical_device::{self, vk_enumerate_physical_devices, vk_get_physical_device_properties, VkPhysicalDevice, VkPhysicalDeviceProperties, VkPhysicalDeviceType, VkQueue, VkQueueFamily, VkQueueFlagBits}};
 
 use super::VulkanApp;
 
@@ -16,8 +16,22 @@ pub struct Device {
 }
 
 
+fn rate_device_type(device_type: VkPhysicalDeviceType) -> u32 {
+    match device_type {
+        VkPhysicalDeviceType::VkPhysicalDeviceTypeOther => 2,
+        VkPhysicalDeviceType::VkPhysicalDeviceTypeIntegratedGpu => 10,
+        VkPhysicalDeviceType::VkPhysicalDeviceTypeDiscreteGpu => 6,
+        VkPhysicalDeviceType::VkPhysicalDeviceTypeVirtualGpu => 8,
+        VkPhysicalDeviceType::VkPhysicalDeviceTypeCpu => 4,
+    }
+}
+
+
 impl VulkanApp {
-    pub(crate) fn create_device(queue_family_qualifiers: Vec<fn(VkQueueFamily) -> bool>) -> Device {
+    pub(crate) fn create_device(&self, queue_family_qualifiers: Vec<fn(VkPhysicalDevice, VkQueueFamily) -> bool>) -> Device {
+        let physical_devices: Vec<VkPhysicalDevice> = vk_enumerate_physical_devices(self.instance);
+
+
         todo!();
     }
 }
