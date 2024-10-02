073823fab7748ceff2f8daff27fddbeec0201869
John-Bush14 <joppe@stuyck.be>
2024-10-02 20:33:06 +0200

    feat: create_native_window

--- forsith-bindings/src/windows/mod.rs
+++ forsith-bindings/src/windows/mod.rs
@@ -2,9 +2,12 @@ use crate::VkSurfaceKHR;
 
 
 pub trait Window {
-    fn initialize_connection() -> Self;
+    fn initialize_connection() -> Self where Self: Sized;
 
     fn map_window(&mut self);
 
     fn create_surface_khr(self) -> VkSurfaceKHR;
 }
+
+
+pub fn create_native_window() -> Box<dyn Window> {todo!();}
