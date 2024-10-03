4d42fa837e969b5d269fdc8f50e8b90e150ff30a
John-Bush14 <joppe@stuyck.be>
2024-10-03 14:22:52 +0200

    feat: CommandPool create func

--- forsith-engine/src/command_pool.rs
+++ forsith-engine/src/command_pool.rs
@@ -1,9 +1,37 @@
-use bindings::command_pool::{VkCommandBuffer, VkCommandPool, VkCommandPoolCreateFlags};
+use bindings::{command_pool::{vk_create_command_pool, VkCommandBuffer, VkCommandPool, VkCommandPoolCreateFlags, VkCommandPoolCreateInfo}, device::VkDevice, physical_device::VkQueueFamily};
 
 
 #[allow(dead_code)]
 pub struct CommandPool {
     command_pool: VkCommandPool,
     command_buffers: Vec<VkCommandBuffer>,
-    flags: VkCommandPoolCreateFlags
+    flags: VkCommandPoolCreateFlags,
+    queue_family: VkQueueFamily
+}
+
+
+impl CommandPool {
+    pub fn new(device: VkDevice, flags: VkCommandPoolCreateFlags, queue_family: VkQueueFamily) -> Self  {
+        let create_info = VkCommandPoolCreateInfo {
+            s_type: VkCommandPoolCreateInfo::structure_type(),
+            p_next: std::ptr::null(),
+            flags: flags.clone(),
+            queue_family
+        };
+
+
+        let mut vk_command_pool = 0;
+
+        vk_create_command_pool(device, &create_info as *const VkCommandPoolCreateInfo, std::ptr::null(), &mut vk_command_pool);
+
+
+        let command_pool = CommandPool {
+            command_pool: vk_command_pool,
+            command_buffers: vec!(),
+            flags,
+            queue_family
+        };
+
+        return command_pool;
+    }
 }
