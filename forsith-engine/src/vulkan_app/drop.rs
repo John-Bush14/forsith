575012f934807a79787ef9a3cc993791ff7ec226
John-Bush14 <joppe@stuyck.be>
2024-10-06 23:43:20 +0200

    feat: destroy transient command pool

--- forsith-engine/src/vulkan_app/drop.rs
+++ forsith-engine/src/vulkan_app/drop.rs
@@ -5,6 +5,8 @@ use super::VulkanApp;
 
 impl Drop for VulkanApp {
     fn drop(&mut self) {
+        self.transient_command_pool.destroy();
+
         self.general_device.destroy().unwrap();
 
         vk_destroy_instance(self.instance, std::ptr::null());
