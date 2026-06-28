bb99d1b8e5072ab0f0103550d83de9bbe4961efd
John-Bush14 <joppe@stuyck.be>
2026-06-28 19:44:11 +0200

    temporarily disable forsith-graphics for later

--- forsith-shared/src/lib.rs
+++ forsith-shared/src/lib.rs
@@ -1,12 +1,20 @@
-use std::ffi::c_void;
 
-pub enum DisplayTarget {
-    Headless,
-    Xorg {display: *const c_void, visual_id: u64},
-    Wayland {wl_display: *const c_void}
 
-}
 
-pub trait GraphicsApi {
-    fn connect(engine_name: &str, engine_version: (u32,u32,u32), target: DisplayTarget) -> Self;
-}
+
+
+
+
+
+// use std::ffi::c_void;
+//
+// pub enum DisplayTarget {
+//     Headless,
+//     Xorg {display: *const c_void, visual_id: u64},
+//     Wayland {wl_display: *const c_void}
+//
+// }
+//
+// pub trait GraphicsApi {
+//     fn connect(engine_name: &str, engine_version: (u32,u32,u32), target: DisplayTarget) -> Self;
+// }
