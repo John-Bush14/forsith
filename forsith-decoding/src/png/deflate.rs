329ae76685ef8e839419b3e4d5b07c916f529eb8
John-Bush14 <joppe@stuyck.be>
2026-08-06 13:29:14 +0200

    refactor: cleaner codelength decoding

--- forsith-decoding/src/png/deflate.rs
+++ forsith-decoding/src/png/deflate.rs
@@ -128,17 +128,20 @@ impl Block {
         let total = hlit as usize + hdist as usize;
         let mut all_codelengths = Vec::with_capacity(total);
 
-        let mut prev = 0;
-
         while all_codelengths.len() < total {
             let symbol = self.codlen_tree.decode_symbol(reader) as u8;
 
             match symbol {
-                0..=15 => {all_codelengths.push(symbol); prev = symbol;},
+                0..=15 => {all_codelengths.push(symbol);},
                 _ => {
-                    let (extra_bits, base, use_prev) = [(2, 3, true), (3, 3, false), (7,11, false)][symbol as usize - 16];
+                    let (extra_bits, base, use_prev) = match symbol {
+                        16 => (2, 3, true),
+                        17 => (3, 3, false),
+                        18 => (7, 11, false),
+                        _ => unreachable!(),
+                    };
 
-                    if !use_prev {prev = 0;}
+                    let prev = if !use_prev {0} else {*all_codelengths.last().unwrap()};
 
                     let repeat = base + reader.read_bits(extra_bits) as usize;
                     all_codelengths.resize(all_codelengths.len() + repeat, prev);
