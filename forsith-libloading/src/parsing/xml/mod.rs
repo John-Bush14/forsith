9bf86a3294d8125ac42abb7ef1543a77b69bf9e2
John-Bush14 <joppe@stuyck.be>
2026-08-26 21:33:38 +0200

    feat: Encoding identification and file decoding into String

--- forsith-libloading/src/parsing/xml/mod.rs
+++ forsith-libloading/src/parsing/xml/mod.rs
@@ -1,9 +1,34 @@
-use std::io::BufRead;
+use std::io::Read;
+use anyhow::{Result, bail};
 
 pub struct XmlDocument;
 
+enum Encoding {
+    Utf8,
+    Utf16LE,
+    Utf16BE,
+}
+
 impl XmlDocument {
-    pub fn parse(xml: impl BufRead) -> Result<XmlDocument, String> {
+    pub fn parse(xml: Vec<u8>) -> Result<XmlDocument> {
+        let start = &xml[..4.min(xml.len())];
+
+        let encoding = match start {
+            [0xEF, 0xBB, 0xBF, _] | [0x3C, 0x3F, 0x78, 0x6D] => Encoding::Utf8,
+            [0xFF, 0xFE, _, _] | [0x3C, 0x00, 0x3F, 0x00] => Encoding::Utf16LE,
+            [0xFE, 0xFF, _, _] | [0x00, 0x3C, 0x00, 0x3F] => Encoding::Utf16BE,
+            _ => {
+                println!("Assuming UTF-8, might be wrong: {start:?}");
+                Encoding::Utf8
+            }
+        };
+
+        let xml = match encoding {
+            Encoding::Utf8 => String::from_utf8(xml)?,
+            Encoding::Utf16LE => String::from_utf16le(&xml)?,
+            Encoding::Utf16BE => String::from_utf16be(&xml)?,
+        };
+
         todo!()
     }
 }
