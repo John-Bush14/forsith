3dce9e641919a79de906bf246c85557bcc5c29d5
John-Bush14 <joppe@stuyck.be>
2026-08-26 19:14:40 +0200

    refactor: remove state machine

--- forsith-libloading/src/lib.rs
+++ forsith-libloading/src/lib.rs
@@ -1,6 +1,3 @@
-#![allow(incomplete_features)]
-#![feature(loop_match)]
-
 #![forbid(unsafe_code)]
 #![warn(clippy::all, clippy::pedantic, clippy::nursery)]
 
--- forsith-libloading/src/parsing/xml/mod.rs
+++ forsith-libloading/src/parsing/xml/mod.rs
@@ -2,28 +2,8 @@ use std::io::BufRead;
 
 pub struct XmlDocument;
 
-enum State {
-    Prologue,
-    Finished,
-}
-
 impl XmlDocument {
     pub fn parse(xml: impl BufRead) -> Result<XmlDocument, String> {
-        let mut state = State::Prologue;
-
-        #[loop_match]
-        'machine: loop {
-            state = 'state: {match state {
-                State::Prologue => {
-                    break 'state State::Finished;
-                },
-
-                State::Finished => {
-                    break 'machine;
-                }
-            }}
-        }
-
         todo!()
     }
 }
